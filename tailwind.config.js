eead800fb691e712f8937f12dd45cfc5fa086895
Thinkedou <r.enguehard@gmail.com>
2023-03-12 18:24:11 +0100

    first

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'card-background': '#eff0f3',
+        'card-background-light': '#fffffe',
+        'button-background': '#ff8e3c',
+        'button-text': '#0d0d0d',
+        'paragraph-black': '#2a2a2a',
+        'tertiary-contrast': '#d9376e'
+      },
+    },
+  },
+  plugins: [],
+}
