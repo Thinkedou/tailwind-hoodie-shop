fc2ee20b23668b73d39c763530cd0e2f5d61dc1c
Thinkedou <r.enguehard@gmail.com>
2023-03-14 09:11:09 +0100

    hoodies pictures and dataset

---
+++ src/assets/js/hoodies.js
@@ -0,0 +1,95 @@
+export const HOODIES = [
+    {
+        ref:'AE47',
+        name:'VUE Hoodie Black',
+        brand:'No brand',
+        price:65,
+        description:'Coupe ajustée, tissu en molleton doux épais, poches sur le devant',
+        availableSize:[
+            'S','M','L','XL'
+        ],
+        globalStock:[
+            {
+                size:'S',
+                stock:3
+            },
+            {
+                size:'M',
+                stock:3
+            },
+            {
+                size:'L',
+                stock:0
+            },
+            {
+                size:'XL',
+                stock:1
+            }
+        ],
+        picture:''
+    },
+    {
+        ref:'AE48',
+        name:'VUE Hoodie Cream',
+        brand:'No brand',
+        price:65,
+        description:'Coupe ajustée, tissu en molleton doux épais, poches sur le devant',
+        availableSize:[
+            'S','M','L','XL'
+        ],
+        globalStock:[
+            {
+                size:'S',
+                stock:0
+            },
+            {
+                size:'M',
+                stock:1
+            },
+            {
+                size:'L',
+                stock:0
+            },
+            {
+                size:'XL',
+                stock:2
+            }
+        ],
+        picture:''
+    },
+    {
+        ref:'RX18',
+        name:'Nike Catapults',
+        brand:'Nike',
+        price:135,
+        description:"Nike Forward catapults your classic hoodie into the future. Unlike traditional knit fabrics, Nike Forward combines multiple thin layers of select fibres for an exceptionally lightweight feel that's effortlessly warm and comfortable.",
+        availableSize:[
+            'S','M','L','XL','2XL'
+        ],
+        globalStock:[
+            {
+                size:'S',
+                stock:3
+            },
+            {
+                size:'M',
+                stock:0
+            },
+            {
+                size:'L',
+                stock:0
+            },
+            {
+                size:'XL',
+                stock:2
+            },
+            {
+                size:'2XL',
+                stock:4
+            }
+        ],
+        picture:''
+    }
+
+
+]
